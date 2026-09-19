475dcabbc1d2c9311d9bc412b69cc464150d4fc0
Mohammed EL KHATTABI <2elkhattabimed2@gmail.com>
2026-09-19 13:18:54 +0200

    setup project, jena dependency and blazegraph

---
+++ src/test/java/com/inrae/ecosemantic/EcosemanticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inrae.ecosemantic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcosemanticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
